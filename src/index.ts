cbbfe087b9975c34ee780c5a30b6c67645ab5d2f
mpatelj <margesh.patelj@gmail.com>
2024-03-23 13:38:23 -0600

    Version without styling
    - Added api endpoint to Env file and then use it in api endpoints for searching the cards
    - Created api endpoint in index file
    - Add searchbar component to fetch cards using search terms
    - Created CardList component to display search results
    - Implement all the components, api endpoints in main app

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
 import express, { Request, Response } from 'express';
+import axios from 'axios';
+import cors from 'cors';
+require('dotenv').config();
 
 const app = express();
 const port = 3001;
+const SCRYFALL_API_URL = process.env.SCRYFALL_API_URL;
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Good luck!');
+// Using the cors middleware
+app.use(cors());
+
+app.get('/cards/search', async (req: Request, res: Response) => {
+  try {
+    const { q } = req.query;
+    if (!q) {
+      return res.status(400).json({ error: 'Search string is required' });
+    }
+    const apiUrl = `${SCRYFALL_API_URL}/cards/search?q=${encodeURIComponent(q.toString())}`;
+    const response = await axios.get(apiUrl);
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching cards:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
   console.log(`Listening on port ${port}.`);
 });
